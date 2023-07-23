1f1cfe123708eef8da7e8535b9955aafe29765d3
Kamel-Marzouk <k.marzouk.amin@gmail.com>
2023-07-23 16:16:27 +0300

    implement tabs

--- src/app/shared/tap/tap.component.ts
+++ src/app/shared/tap/tap.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tap',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./tap.component.css']
 })
 export class TapComponent {
+  @Input() tabTitle: string = '';
+  @Input() active: boolean = false;
 
 }
--- src/app/shared/taps-container/taps-container.component.ts
+++ src/app/shared/taps-container/taps-container.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, ContentChildren, AfterContentInit, QueryList } from '@angular/core';
+import { TapComponent } from '../tap/tap.component';
 @Component({
   selector: 'app-taps-container',
   templateUrl: './taps-container.component.html',
   styleUrls: ['./taps-container.component.css']
 })
-export class TapsContainerComponent {
+export class TapsContainerComponent implements AfterContentInit {
 
+  @ContentChildren(TapComponent) tabs: QueryList<TapComponent> = new QueryList();
+
+  ngAfterContentInit(): void {
+    const activeTabs = this.tabs?.filter((tab: TapComponent) => tab.active);
+    if (!activeTabs || !activeTabs.length) this.selectTab(this.tabs!.first);
+  }
+
+  selectTab(tab: TapComponent): void {
+    this.tabs?.forEach((tab: TapComponent) => tab.active = false);
+    tab.active = true;
+  }
 }
