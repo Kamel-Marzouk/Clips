519ca96211bc35bbc03846d00a48f2e1d7d78115
Kamel-Marzouk <k.marzouk.amin@gmail.com>
2023-07-27 15:31:21 +0300

    implement login form by using reactive form

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  email: FormControl = new FormControl('', [
+    Validators.required,
+    Validators.email
+  ]);
+  password: FormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+  ]);
 
+  loginForm: FormGroup = new FormGroup({
+    email: this.email,
+    password: this.password
+  });
+
+  showAlert: boolean = false;
+  alertColor: string = 'blue';
+  alertMessage: string = 'You are logged in successfully';
+
+  login(): void {
+    this.showAlert = true;
+    this.alertColor = 'blue';
+    this.alertMessage = 'You are logged in successfully';
+  }
 }
