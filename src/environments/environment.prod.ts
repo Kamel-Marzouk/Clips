2b6f2ad2e98e193782c8fd0bbeb05a0f221e00b9
Kamel-Marzouk <k.marzouk.amin@gmail.com>
2023-07-31 01:56:32 +0300

    add environments and import AngularFireModule and AngularFireAuthModule

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDRDpi8M2gOli79N5KixO_blitNKsO-7tU",
+    authDomain: "clips-220123.firebaseapp.com",
+    projectId: "clips-220123",
+    storageBucket: "clips-220123.appspot.com",
+    appId: "1:227089640705:web:c927c286776122663bff00"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDRDpi8M2gOli79N5KixO_blitNKsO-7tU",
+    authDomain: "clips-220123.firebaseapp.com",
+    projectId: "clips-220123",
+    storageBucket: "clips-220123.appspot.com",
+    appId: "1:227089640705:web:c927c286776122663bff00"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
